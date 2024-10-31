a9d9f80420ce9c4c74e258f357705bb4be64a63a
FatinIshraq4 <fatini5@nycstudents.net>
2024-10-31 15:58:41 +0000

    hi

---
+++ Lab4/Code.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Code {
+    public static void main(String[] args) {
+        int p = 5;
+int q = 0;
+
+if (p > 0 && (p / q) == 2) {
+  System.out.println("beep");
+} else {
+  System.out.println("boop");
+}
+
+    }
+}
+
